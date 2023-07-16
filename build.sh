6622cab7eaa8a3a1f32876ee2a95425d09f4366d
Fangjun Kuang <csukuangfj@gmail.com>
2023-07-16 15:11:02 +0800

    small fixes

---
+++ build.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+
+set -ex
+
+
+if [ -z $PYTHON_VERSION ]; then
+  echo "Please set the environment variable PYTHON_VERSION"
+  echo "Example: export PYTHON_VERSION=3.8"
+  # Valid values: 3.8, 3.9, 3.10, 3.11
+  exit 1
+fi
+
+if [ -z $CUDA_VERSION ]; then
+  echo "Please set the environment variable CUDA_VERSION"
+  echo "Example: export CUDA_VERSION=10.2"
+  # valid values: 10.2, 11.1, 11.3, 11.6, 11.7, 11.8
+  exit 1
+fi
+
+if [ -z $TORCH_VERSION ]; then
+  echo "Please set the environment variable TORCH_VERSION"
+  echo "Example: export TORCH_VERSION=1.10.0"
+  exit 1
+fi
+
+echo "Installing ${PYTHON_VERSION}.1"
+
+yum -y install openssl-devel bzip2-devel libffi-devel xz-devel wget
+
+curl -O https://www.python.org/ftp/python/${PYTHON_VERSION}.1/Python-${PYTHON_VERSION}.1.tgz
+tar xf Python-${PYTHON_VERSION}.1.tgz
+pushd Python-${PYTHON_VERSION}.1
+
+PYTHON_INSTALL_DIR=$PWD/py-${PYTHON_VERSION}
+./configure --enable-optimizations --enable-shared --prefix=$PYTHON_INSTALL_DIR
+make install
+
+export PATH=$PYTHON_INSTALL_DIR/bin:$PATH
+export LD_LIBRARY_PATH=$PYTHON_INSTALL_DIR/lib:$LD_LIBRARY_PATH
+
+popd
+
+python3 --version
+which python3
+
+python3 -m pip install -U pip cmake
+# python3 -m pip install torch==${TORCH_VERSION} cmake
+python3 -m pip install wheel twine typing_extensions
+python3 -m pip install bs4 requests tqdm
+
+echo "Installing torch"
+./install_torch.sh
+
+echo "Downloading k2"
+git clone --depth 1 https://github.com/k2-fsa/k2
+cd k2
+
+export CMAKE_CUDA_COMPILER_LAUNCHER=
+export K2_CMAKE_ARGS=" -DPYTHON_EXECUTABLE=$PYTHON_INSTALL_DIR/bin/python3 "
+export K2_MAKE_ARGS=" -j1 "
+
+python3 setup.py bdist_wheel
+
+cp ./dist/*.whl /var/www
+
+auditwheel repair \
+  --exclude libc10.so \
+  --exclude libc10_cuda.so \
+  --exclude libcuda.so.1 \
+  --exclude libcudart.so.${CUDA_VERSION} \
+  --exclude libnvToolsExt.so.1 \
+  --exclude libnvrtc.so.${CUDA_VERSION} \
+  --exclude libtorch.so \
+  --exclude libtorch_cpu.so \
+  --exclude libtorch_cuda.so \
+  --exclude libtorch_python.so \
+  --plat manylinux_2_17_x86_64 \
+  -w ../var/www \
+  *.whl
---
+++ generate_build_matrix.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python3
+# Copyright    2022  Xiaomi Corp.        (authors: Fangjun Kuang)
+
+import argparse
+import json
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--enable-cuda",
+        action="store_true",
+        default=False,
+        help="True to enable CUDA",
+    )
+
+    parser.add_argument(
+        "--test-only-latest-torch",
+        action="store_true",
+        default=False,
+        help="""If True, we test only the latest PyTorch
+        to reduce CI running time.""",
+    )
+    return parser.parse_args()
+
+
+def generate_build_matrix(enable_cuda, test_only_latest_torch):
+    matrix = {
+        # there are issues in serializing ragged tensors in 1.5.0 and 1.5.1
+        #  "1.5.0": {
+        #      "python-version": ["3.6", "3.7", "3.8"],
+        #      "cuda": ["10.1", "10.2"],
+        #  },
+        #  "1.5.1": {
+        #      "python-version": ["3.6", "3.7", "3.8"],
+        #      "cuda": ["10.1", "10.2"],
+        #  },
+        "1.6.0": {
+            "python-version": ["3.6", "3.7", "3.8"],
+            "cuda": ["10.1", "10.2"],
+        },
+        "1.7.0": {
+            "python-version": ["3.6", "3.7", "3.8"],
+            "cuda": ["10.1", "10.2", "11.0"],  # default 10.2
+        },
+        "1.7.1": {
+            "python-version": ["3.6", "3.7", "3.8", "3.9"],
+            "cuda": ["10.1", "10.2", "11.0"],  # default 10.2
+        },
+        "1.8.0": {
+            "python-version": ["3.6", "3.7", "3.8", "3.9"],
+            "cuda": ["10.1", "10.2", "11.1"],  # default 10.2
+        },
+        "1.8.1": {
+            "python-version": ["3.6", "3.7", "3.8", "3.9"],
+            "cuda": ["10.1", "10.2", "11.1"],  # default 10.2
+        },
+        "1.9.0": {
+            "python-version": ["3.6", "3.7", "3.8", "3.9"],
+            "cuda": ["10.2", "11.1"],  # default 10.2
+        },
+        "1.9.1": {
+            "python-version": ["3.6", "3.7", "3.8", "3.9"],
+            "cuda": ["10.2", "11.1"],  # default 10.2
+        },
+        "1.10.0": {
+            "python-version": ["3.6", "3.7", "3.8", "3.9"],
+            "cuda": ["10.2", "11.1", "11.3"],
+        },
+        "1.10.1": {
+            "python-version": ["3.6", "3.7", "3.8", "3.9"],
+            "cuda": ["10.2", "11.1", "11.3"],  # default 10.2
+        },
+        "1.10.2": {
+            "python-version": ["3.6", "3.7", "3.8", "3.9"],
+            "cuda": ["10.2", "11.1", "11.3"],  # default 10.2
+        },
+        "1.11.0": {
+            "python-version": ["3.7", "3.8", "3.9", "3.10"],
+            "cuda": ["10.2", "11.3", "11.5"],  # default 10.2
+        },
+        "1.12.0": {
+            "python-version": ["3.7", "3.8", "3.9", "3.10"],
+            "cuda": ["10.2", "11.3", "11.6"],  # default 10.2
+        },
+        "1.12.1": {
+            "python-version": ["3.7", "3.8", "3.9", "3.10"],
+            "cuda": ["10.2", "11.3", "11.6"],  # default 10.2
+        },
+        "1.13.0": {
+            "python-version": ["3.7", "3.8", "3.9", "3.10", "3.11"],
+            "cuda": ["11.6", "11.7"],  # default 11.7
+        },
+        "1.13.1": {
+            "python-version": ["3.7", "3.8", "3.9", "3.10", "3.11"],
+            "cuda": ["11.6", "11.7"],  # default 11.7
+        },
+        "2.0.0": {
+            "python-version": ["3.8", "3.9", "3.10", "3.11"],
+            "cuda": ["11.7", "11.8"],  # default 11.7
+        },
+        "2.0.1": {
+            "python-version": ["3.8", "3.9", "3.10", "3.11"],
+            "cuda": ["11.7", "11.8"],  # default 11.7
+        },
+    }
+    if test_only_latest_torch:
+        latest = "2.0.1"
+        matrix = {latest: matrix[latest]}
+
+    # We only have limited spaces in anaconda, so we exclude some
+    # versions of PyTorch here. If you need them, please consider
+    # installing k2 from source
+    # Only CUDA build are excluded since it occupies more disk space
+    excluded_torch_versions = ["1.6.0", "1.7.0", "1.7.1", "1.8.0", "1.8.1"]
+    excluded_torch_versions += ["1.9.0", "1.9.1"]
+
+    excluded_python_versions = ["3.6", "3.7"]
+    enabled_cuda_versions = ["10.2", "11.6", "11.7", "11.8"]
+
+    ans = []
+    for torch, python_cuda in matrix.items():
+        if torch in excluded_torch_versions and enable_cuda:
+            continue
+
+        python_versions = python_cuda["python-version"]
+        cuda_versions = python_cuda["cuda"]
+
+        if enable_cuda:
+            for p in python_versions:
+                if p in excluded_python_versions:
+                    continue
+                for c in cuda_versions:
+                    if enabled_cuda_versions and c not in enabled_cuda_versions:
+                        continue
+                    ans.append(
+                        {
+                            "torch": torch,
+                            "python-version": p,
+                            "cuda": c,
+                            "image": f"pytorch/manylinux-builder:cuda{c}",
+                        }
+                    )
+        else:
+            for p in python_versions:
+                ans.append({"torch": torch, "python-version": p})
+
+    print(json.dumps({"include": ans}))
+
+
+def main():
+    args = get_args()
+    generate_build_matrix(
+        enable_cuda=args.enable_cuda,
+        test_only_latest_torch=args.test_only_latest_torch,
+    )
+
+
+if __name__ == "__main__":
+    main()
